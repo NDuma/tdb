1a69ff4346cae973df680593f4d6ce5b8d17f9df
Luiz Sócrate <luiz@socrate.com.br>
2015-04-18 22:47:15 -0300

    Use pods

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'techdb-admin',
+    podModulePrefix: 'techdb-admin/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
