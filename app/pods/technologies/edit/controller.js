41e8ac1fc0c4a03080951c8eb1c639aecfebb615
Luiz Sócrate <luiz@socrate.com.br>
2015-05-03 23:21:33 -0300

    Technology edit route

---
+++ app/pods/technologies/edit/controller.js
@@ -0,0 +1,19 @@
+import Ember from 'ember'
+import Tech from 'techdb-admin/models/tech'
+import _ from 'npm:lodash'
+import hmean from 'npm:compute-hmean'
+
+const { computed, getProperties } = Ember
+const { QUESTIONS } = Tech
+const ALL_QUESTIONS = QUESTIONS.join(',')
+const MODEL_QUESTIONS = `model.{${ALL_QUESTIONS}}`
+
+export default Ember.Controller.extend({
+  currentReadiness: computed(MODEL_QUESTIONS, {
+    get () {
+      let model = this.get('model')
+      let questions = _.values(getProperties(model, QUESTIONS))
+      return hmean(questions).toFixed(1)
+    }
+  })
+})
