b2495307765f1cbe992702653979ce79d3a84f17
Luiz Sócrate <luiz@socrate.com.br>
2015-05-10 23:48:38 -0300

    Add tech delete

--- app/pods/technologies/index/route.js
+++ app/pods/technologies/index/route.js
@@ -1,5 +1,9 @@
 import Ember from 'ember'
 
 export default Ember.Route.extend({
-  model () { return this.store.find('tech') }
+  model () { return this.store.find('tech') },
+
+  actions: {
+    delete (model) { model.destroyRecord() }
+  }
 })
