ac512af0c71992ee3452d2fbeb503711de96b2dc
Luiz Sócrate <luiz@socrate.com.br>
2015-05-16 20:42:55 -0300

    streamline the router

--- app/router.js
+++ app/router.js
@@ -6,11 +6,11 @@ var Router = Ember.Router.extend({
 })
 
 export default Router.map(function() {
-  this.route('register');
-  this.route('signin');
-  this.resource('technologies', function() {
-    this.route('read', { path: ':tech_id' });
-    this.route('edit', { path: ':tech_id/edit' });
-    this.route('create');
-  });
+  this.route('register')
+  this.route('signin')
+  this.route('technologies', function() {
+    this.route('read', { path: ':tech_id' })
+    this.route('edit', { path: ':tech_id/edit' })
+    this.route('create')
+  })
 })
