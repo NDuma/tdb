013bdfe281971e67c744db04aab42fedcaf8d606
Luiz Sócrate <luiz@socrate.com.br>
2015-06-18 16:14:41 -0300

    Readiness index

--- app/router.js
+++ app/router.js
@@ -7,10 +7,11 @@ var Router = Ember.Router.extend({
 
 export default Router.map(function() {
   this.route('register')
-  this.route('signin')
+  this.route('signin');
   this.route('technologies', function() {
     this.route('read', { path: ':tech_id' })
     this.route('edit', { path: ':tech_id/edit' })
     this.route('create')
-  })
+  });
+  this.route('readiness', function() {})
 })
