a63c3f82805255c291b8f4224ac76ef63b005463
Luiz Sócrate <luiz@socrate.com.br>
2015-07-01 16:22:32 -0300

    Update title tag on navigation
    Closes #27

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -1,4 +1,23 @@
 import Ember from 'ember'
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin'
 
-export default Ember.Route.extend(ApplicationRouteMixin)
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  updateAppTitle () {
+    let path = this.get('controller.currentPath')
+    let model = this.modelFor(path)
+    let title
+    if (model) {
+      let modelName = model.get('type.modelName')
+      let where = Ember.isArray(model) ? `All ${modelName}`: model.get('name')
+      title = `TDB - ${where}`
+    } else {
+      title = `TDB`
+    }
+    document.getElementsByTagName('title')[0].text = title
+  },
+  actions: {
+    didTransition () {
+      Ember.run.next(this, this.updateAppTitle)
+    }
+  }
+})
